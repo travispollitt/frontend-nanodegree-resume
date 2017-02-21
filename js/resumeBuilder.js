b5dde670f35a225f67dc4dddf2527162be3f17c5
travispollitt <travis.pollitt@ge.com>
2017-02-21 11:29:30 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,5 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Travis Pollitt");
+var awesomeThoughts = "I am Travis and I am awesome"
+console.log(awesomeThoughts);
